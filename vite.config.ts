a505d3cf5904099603246be57321274ed2fd0920
Fabien Sanchez <fabien.sanchez@citopia.fr>
2025-12-02 16:57:09 +0100

    chore: add base root for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/anti-ninja-device/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+});
